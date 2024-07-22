8050336aa927f56a0070ec510982e915fbc28ba7
XBigTK13X <xbigtk13x@gmail.com>
2024-07-22 08:29:19 -0500

    Able to reproduce the crash from snowstream in the example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,8 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LibmpvVideo } from 'react-native-libmpv';
-
-export default function App() {
-  const surfaceRef = React.useRef(null);
-  const [playing, setPlaying] = React.useState(false)
-  const mkvUrl = "http://192.168.1.20:9064/media/movies/testing/Ocean's Eleven (2001)/Ocean's Eleven (2001) WEBDL-480p.mkv"
-  const frigateUrl = "http://192.168.1.20:8000/api/streamable/direct?streamable_id=58"
-  const hdHomeRunUrl = "http://192.168.1.20:8000/api/streamable/direct?streamable_id=1"
-  const hdHomeRunUrlTrans = "http://192.168.1.20:8000/api/streamable/transcode?streamable_id=1"
-  const iptvUrl = "http://192.168.1.20:8000/api/streamable/direct?streamable_id=124"
-
-  function onLibmpvEvent(libmpvEvent) {
-    console.log({ libmpvEvent })
-  }
-
-  return (
-    <View style={styles.container}>
-      <LibmpvVideo playUrl={frigateUrl} onLibmpvEvent={onLibmpvEvent}></LibmpvVideo>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -35,3 +16,41 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   }
 });
+
+function LandingPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Button onPress={() => { navigation.navigate('Video') }} title="Play Video" />
+    </View>
+  )
+}
+
+function VideoPage() {
+  function onLibmpvEvent(libmpvEvent) {
+    console.log({ libmpvEvent })
+  }
+  const videoUrl = 'http://192.168.1.5:5004/auto/v4.1'
+  return (
+    <View style={styles.container}>
+      <LibmpvVideo playUrl={videoUrl} onLibmpvEvent={onLibmpvEvent}></LibmpvVideo>
+    </View>
+  )
+}
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  //const surfaceRef = React.useRef(null);
+  //const [playing, setPlaying] = React.useState(false)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={LandingPage} />
+        <Stack.Screen name="Video" component={VideoPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
